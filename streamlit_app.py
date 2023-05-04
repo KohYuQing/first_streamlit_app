8b5581c1aa23000a22a3ef1f0feefaea533607a7
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-05-04 13:31:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.markdown('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.markdown('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.markdown('🐔 Hard-Boiled Free-Range Egg')
 streamlit.markdown('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
