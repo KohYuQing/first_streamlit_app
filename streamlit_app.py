a0c7ef7db42a1680b24f2a5dda8b81414d038761
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-05-04 13:13:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.subheader('Breakfast Menu')
 streamlit.markdown('Omega 3 & Blueberry Oatmeal')
+streamlit.markdown('Kale, Spinach & Rocket Smoothie')
+streamlit.markdown('Hard-Boiled Free-Range Egg')
